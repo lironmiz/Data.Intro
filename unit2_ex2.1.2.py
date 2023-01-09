927a7260fa6af04a5182c00b34ff5c712b5ece8e
Liron-Mizrahi <91504420+lironmiz@users.noreply.github.com>
2023-01-09 13:16:06 +0200

    Create unit2_ex2.1.2.py

---
+++ unit2_ex2.1.2.py
@@ -0,0 +1,31 @@
+# exercise 2.1.2 from unit 2
+'''
+An airline wants to analyze all the cases in which a passenger reported the loss of luggage that was sent to the belly of the plane. The company decided to build a table that would gather the data. Below is an excerpt from the table:
+
+Date Flight number Origin port Destination port Type of cargo located/not located Amount of compensation
+28.3 YY580 Tel Aviv Paris Suitcase Location 0
+11.4 YY630 Barcelona Tel Aviv unusual not located $550
+13.4 YY720 Tel Aviv Berlin Bag Location 0
+'''
+
+# 1: The line where the fields 'date', 'flight number', 'origin port', 'destination port' and so on appear is:
+
+# Answer: the column headings
+
+# 2: The fields containing the information 'Paris', 'Tel Aviv' and 'Berlin' are fields:
+
+# Answer: feature
+
+# 3:The fields containing the information 'YY630', 'Barcelona', and 'Tel Aviv' are fields:
+
+# Answer: record
+
+'''
+Select the option that lists actions that are not supported in pandas:
+
+Choose all the correct answers:
+'''
+
+# 1: Increasing the font in a certain column
+
+# 2: Decrease the font in a certain column
