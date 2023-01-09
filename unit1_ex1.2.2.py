94a56eab315f703dd967049fbeed80d03a629ab9
Liron-Mizrahi <91504420+lironmiz@users.noreply.github.com>
2023-01-09 12:58:38 +0200

    Create unit1_ex1.2.2.py

---
+++ unit1_ex1.2.2.py
@@ -0,0 +1,21 @@
+# exercise 1.2.2 from unit 1
+'''
+Here are two lists: a list of variables from a database describing books in English and a 
+list of variable types. Match each variable with its variable type.
+'''
+
+# 1: Reading level (in terms of English, for example 'easy' or 'difficult')
+
+# Answer:  ordinal
+
+# 2: name of the book 
+
+# Answer:  free text
+
+# 3: Literary genre
+
+# Answer: categorical
+
+# 3: number of pages 
+
+# Answer: numerical 
