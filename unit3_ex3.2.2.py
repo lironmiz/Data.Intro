a4090e3fa6119d1dcf3227bd87d9eca9914ef356
Liron-Mizrahi <91504420+lironmiz@users.noreply.github.com>
2023-01-10 13:13:21 +0200

    Create unit3_ex3.2.2.py

---
+++ unit3_ex3.2.2.py
@@ -0,0 +1,22 @@
+# exercise 3.2.2 from unit 3
+'''
+For a certain list of songs, the following frequency table was obtained (using value_counts):
+
+ styles.value_counts()
+Bossa Nova 7
+Samba 6
+Reggae 4
+Jazz 3
+dtype: int64
+Which of the following answers is the correct result for calculating the relative frequency (in other words - what will be the result of pd.value_counts(styles, normalize=True)?
+You can use a calculator to solve the question
+'''
+
+# Answer: 
+'''
+Bosa nova 0.35
+Samba 0.30
+Reggae 0.20
+Jazz 0.15
+dtype: float64
+'''
