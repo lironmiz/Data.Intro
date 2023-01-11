9660634deb1102c405e68e58d63304c203a77b75
Liron-Mizrahi <91504420+lironmiz@users.noreply.github.com>
2023-01-11 04:05:30 +0200

    Create unit4_ex4.1.7.py

---
+++ unit4_ex4.1.7.py
@@ -0,0 +1,34 @@
+# exercise 4.1.7 from unit 4
+'''
+Moshe and Aviva both worked on the same list of data. They both decided to calculate a histogram and display it as a chart.
+However, as you can see in the following figures,
+each of them chose to group the data in a different way.
+'''
+# link to the histograms: https://courses.campus.gov.il/courses/course-v1:CS+GOV_CS_Data_DataIntro101_HE+2022_2/courseware/cdebd807bf75410bb99c958796a83a38/74ed72c9990c4c299bf6a4de28052a66/
+
+'''
+They both submitted their charts to Eric. After examining them, Eric argued that the histograms did not match each other, and that 
+they should recheck the charts they had created. After checking, Moshe found that there was indeed a
+mistake in the diagram he prepared, and submitted a corrected diagram.
+Please note: in this question the list of data from which the histograms were built is not given. All
+we know is that the histograms of Moshe and Aviva refer to the same list.
+'''
+
+# 1: 
+'''
+Which column in the diagram did Moshe correct?
+'''
+
+# Answer: column A
+
+# 2: 
+'''
+Following the previous question, mark the correct option:
+
+Moshe corrected column A with this?
+'''
+
+# Answer: reduced it
+
+
+
