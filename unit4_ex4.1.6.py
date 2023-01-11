39c7f0fa29475c811fdc375653729e408c96032d
Liron-Mizrahi <91504420+lironmiz@users.noreply.github.com>
2023-01-11 03:54:00 +0200

    Create unit4_ex4.1.6.py

---
+++ unit4_ex4.1.6.py
@@ -0,0 +1,54 @@
+# exercise 4.1.6 from unit 4
+'''
+The table in front of you is a histogram (grouped frequency table) of album lengths, in minutes, of a certain band:
+
+frequency range
+1 30–34
+3 35–39
+5 40–44
+2 45–50
+It is also known that:
+
+The lowest value in the range (minimum) is 31, meaning the shortest album length is 31 minutes
+The highest value in the range (max) is 46
+All data are integers
+'''
+# 1: 
+'''
+In each of the following sections there is a section of data. For each section,
+check whether it is possible that it was taken from the list of album lengths, from which the frequency
+table was calculated. If so, mark 'true', if not, mark 'false'.
+'''
+# 1.1 - 35,36,37,38,39
+
+# Answer: false 
+
+# 1.2 - 35,37,39
+
+# Answer: true
+
+# 1.3 - 35,35,35
+
+# Answer: true
+
+# 2:
+'''
+In what range is the median?
+'''
+
+# Answer: 40 - 44
+
+# 3: 
+'''
+What is the frequency of the value 33 in the data list? Choose the most correct answer:
+'''
+
+# Answer: The value 33 does not appear in the data list
+
+# 4: 
+'''
+What is the frequency of the value 45 in the data? Choose the most correct answer:
+'''
+
+# Answer: The value 45 appears at most once in the data list
+
